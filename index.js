6582d4641fb5de7e8e48553e90e26fa2264e05db
Govind <you@example.com>
2024-07-18 10:11:28 +0530

    first_commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const app = express();
+dotenv.config(); 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger("dev"));
+app.get('/', (req, res) => {
+  res.send("server is running on AWS");
+});
+
+app.listen(8080, () => {
+  console.log(`Server is running on port ${8080}`);
+});
